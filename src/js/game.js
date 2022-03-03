c5fc01937a27ac3c865684a3c9181d3919d7a2c4
Mucharski <igor.m99@hotmail.com>
2022-03-03 16:49:51 -0300

    jogo finalizado com todas verificações

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,23 @@
-const FRONT = "frente-carta";
-const BACK = "atras-carta";
+let btnRestart = document.querySelectorAll(".btn-restart");
+btnRestart.forEach(element => {
+    element.addEventListener("click", () => {
+        game.restartGame();
+    })
+})
+
+const btnIniciar = document.getElementById("btnIniciar");
+btnIniciar.addEventListener("click", () => {
+    iniciarJogo();
+
+    let startDiv = document.getElementById("start");
+    startDiv.style.display = "none";
+});
 
 let game = {
     lockMode: false,
     firstCard: null,
     secondCard: null,
+    tentativas: 20,
 
     setCard: (id) => {
         let card = cards.filter(card => card.id == id)[0];
@@ -29,6 +42,13 @@ let game = {
     checkGame: () => {
         if (game.firstCard != null && game.secondCard != null && game.firstCard.icon == game.secondCard.icon) {
             return game.firstCard.icon == game.secondCard.icon
+        } else if (game.tentativas == 0) {
+        }
+    },
+
+    checkChances: () => {
+        if (game.tentativas == 0) {
+            document.getElementById("gameOver").style.display = "flex";
         }
     },
 
@@ -48,36 +68,16 @@ let game = {
 
 }
 
-let champs = [
-    "Ekko",
-    "Elise",
-    "Jinx",
-    "Kayn",
-    "Kindred",
-    "Nasus",
-    "Rhaast",
-    "Viego",
-    "Warwick",
-    "Zed"
-] // todos campeões que vão estar nas cartas
+let cards = [], champs = ["Ekko", "Elise", "Jinx", "Kayn", "Kindred", "Nasus", "Rhaast", "Viego", "Warwick", "Zed"] // todos campeões que vão estar nas cartas
 
-let cards = [];
 
-const createCardsPair = (champs) => { // criar 2 cartas do mesmo campeão mas com ids diferentes
-    return [{
-        id: createId(champs),
-        icon: `../icons/${champs}Square.png`,
-        flipped: false,
-    }, {
-        id: createId(champs),
-        icon: `../icons/${champs}Square.png`,
-        flipped: false,
-    }]
-}
+const iniciarJogo = () => {
 
-const createId = (champs) => {
-
-    return champs + parseInt(Math.random() * 1000); // retorna um id aleatorio entre 0 e 999
+    let tentativas = document.getElementById("tentativa");
+    tentativas.innerHTML = game.tentativas;
+    cards = createCards(champs);
+    embaralharCartas(cards);
+    renderizarCartas(cards);
 
 }
 
@@ -88,9 +88,31 @@ const createCards = (champs) => {
     });
 
     return cards.flatMap(card => card); // retorna o array desmembrando a "dupla" de cartas por causa da função flatMap
+
+}
+
+const createId = (champ) => {
+
+    return champ + parseInt(Math.random() * 1000); // retorna um id aleatorio entre 0 e 999
+
+}
+
+const createCardsPair = (champs) => { // criar 2 cartas do mesmo campeão mas com ids diferentes
+
+    return [{
+        id: createId(champs),
+        icon: `../icons/${champs}Square.png`,
+        flipped: false,
+    }, {
+        id: createId(champs),
+        icon: `../icons/${champs}Square.png`,
+        flipped: false,
+    }];
+
 }
 
 const embaralharCartas = (cards) => {
+
     let cartaAtual = cards.length;
     let aleatorio = 0;
 
@@ -101,35 +123,38 @@ const embaralharCartas = (cards) => {
         [cards[aleatorio], cards[cartaAtual]] = [cards[cartaAtual], cards[aleatorio]]; // enquanto tiver passando pelas cartas, inverte o valor da primeira com a última até chegar a 0
         // as cartas serão aleatórias graças a variavel aleatorio que criar um valor entre 0 e 19 para que a carta invertida pela última seja sempre aleatória
     }
+
 }
 
-const iniciarJogo = () => {
-    cards = createCards(champs);
-    embaralharCartas(cards);
-    renderizarCartas(cards)
-}
 
 const flipCard = (element) => {
+
     if (game.setCard(element.id)) {
 
         element.classList.add("flip");
 
-        if (game.checkGame.bind(game)()) {
+        if (game.checkGame.bind(game)()) { // se tem as duas cartas viradas checa o jogo
+
             game.clearCards();
+
             let acertou = document.getElementById("acertou");
             acertou.style.display = "flex";
 
             setTimeout(() => {
                 acertou.style.display = "none"
-            }, 1000)
-            // fazer função que aparece se foi par ou não
-            if (game.checkEndGame()) {
+            }, 1000);
+
+            if (game.checkEndGame()) { // se retornar true pro fim de jogo
+
                 let endLayout = document.getElementById("endGame");
                 endLayout.style.display = "flex";
 
             }
+
         } else {
+
             if (game.secondCard) {
+
                 let errou = document.getElementById("errou");
                 errou.style.display = "flex";
 
@@ -143,30 +168,23 @@ const flipCard = (element) => {
                     firstCard.classList.remove("flip");
                     secondCard.classList.remove("flip");
 
-
-
                     errou.style.display = "none"
+
                     game.clearCards();
-                }, 700)
+
+                    game.tentativas -= 1;
+                    let tentativas = document.getElementById("tentativa");
+                    tentativas.innerHTML = game.tentativas;
+
+                    game.checkChances();
+
+                }, 700) // espera 0.7 segundos para desvirar as cartas
+
             };
         }
-
     }
 }
 
-let btnRestart = document.getElementById("btnRestart");
-
-btnRestart.addEventListener("click", () => {
-    game.restartGame();
-})
-
-let btnIniciar = document.getElementById("btnIniciar");
-btnIniciar.addEventListener("click", () => {
-    iniciarJogo();
-
-    let startDiv = document.getElementById("start");
-    startDiv.style.display = "none";
-})
 
 
 
